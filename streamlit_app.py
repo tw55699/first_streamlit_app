3145abe0493d5d5aa1e542291060208823277260
tw55699 <127403271+tw55699@users.noreply.github.com>
2023-03-09 12:36:55 +0000

    Update streamlit_app.py
    Added Emoji
    Added 1 more breakfast menu item
    Added 1 new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard boiled Free-Range egg')
+streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard boiled Free-Range egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
